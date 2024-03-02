9f3242bbdc57e6471d2720cc889d765b44f4f747
Skye Burn <skye@kali.dj-kali.com>
2024-03-02 12:33:46 +0200

    add vulnerability-systems.txt

---
+++ learn/install_docker_for_kali.sh
@@ -0,0 +1,8 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg >/dev/null
+
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
---
+++ learn/web-apps/juice-shop/deploy.sh
@@ -0,0 +1,3 @@
+sudo docker pull bkimminich/juice-shop
+sudo docker runn -rm -p 3000:3000 bkimminich/juice-shop # will fail. Run separately in terminal
+
